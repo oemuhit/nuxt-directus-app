7679b64f88135ef4b0f9c75e1ff0b38cc149c162
Osman Ertuğrul MUHİT <osmanertugrul.muhit@ogr.iu.edu.tr>
2025-10-27 11:57:07 +0300

    cloud

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,11 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   ssr: true,
-
+  /*   nitro: {
+    prerender: {
+      autoSubfolderIndex: false,
+    },
+  }, */
   components: [
     {
       path: "~/components/ui",
